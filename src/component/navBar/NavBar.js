2afa00a6bd1df1d86e56374fcaaee4c51001183a
Liza Benitez <leonetkd@gmail.com>
2022-02-11 19:17:24 -0300

    NavBar Listo

--- src/component/navBar/NavBar.js
+++ src/component/navBar/NavBar.js
@@ -2,13 +2,13 @@ import "./NavBar.css";
 import {Link} from 'react-router-dom';
 function NavBar(props) {
     return (
-        <nav>  
+        <nav className="R">  
             <ul className="roberto">
-            <li ><Link to="/"><img className="logo" src="https://www.dafont.com/forum/attach/orig/3/6/366983.png" alt="logo" width={"16%"}/></Link></li>
-            <li ><Link className="item"to="/">Home</Link></li>
-            <li ><Link className="item"to="/category/:id">Categoria</Link></li>
-            <li ><Link className="item"to="/">Info</Link></li>
-            <li ><Link  className="item"to="/">Contactos</Link></li>
+            <li className="item"><Link to="/"><img className="logo" src="https://www.dafont.com/forum/attach/orig/3/6/366983.png" alt="logo" width={"16%"}/></Link></li>
+            <li className="item"><Link className="item"to="/">Home</Link></li>
+            <li className="item"><Link className="item"to="/category/:id">Categoria</Link></li>
+            <li className="item"><Link className="item"to="/">Info</Link></li>
+            <li className="item" ><Link  className="item"to="/">Contactos</Link></li>
             </ul>
         </nav>
     );
